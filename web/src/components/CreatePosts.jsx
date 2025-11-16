9bc5707aaa72bbbb1d00716006ccb082fa22dd87
celse93 <pamonteiro1@gmail.com>
2025-11-16 17:45:51 +0100

    CreatePost style done

--- web/src/components/CreatePosts.jsx
+++ web/src/components/CreatePosts.jsx
@@ -1,23 +1,30 @@
 import { useState, useContext } from 'react';
 import { getBooksSearch, postBook } from '../services/api/books';
 import { UserContext } from '../context/UserContext';
-import { useParams } from 'react-router';
 import {
   Box,
   Button,
-  FormControl,
-  InputLabel,
   List,
   ListItem,
+  ListItemButton,
   MenuItem,
   Modal,
   Select,
-  TextareaAutosize,
   TextField,
   Typography,
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import { width } from '@mui/system';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
 export const CreatePosts = () => {
   const initialBookState = {
@@ -36,9 +43,9 @@ export const CreatePosts = () => {
   const [content, setContent] = useState('');
   const [open, setOpen] = useState(false);
   const { fetchFeedData, fetchUserFeed, categories } = useContext(UserContext);
-  let { profileId } = useParams();
+  const [anchorEl, setAnchorEl] = useState(null);
 
-  const handleSearch = async () => {
+  const handleSearch = async (e) => {
     if (!query.trim()) {
       alert('Error: Search query is empty.');
       return;
@@ -46,10 +53,12 @@ export const CreatePosts = () => {
     try {
       const result = await getBooksSearch(query);
       setBooks(result);
+      setAnchorEl(e.target);
     } catch (error) {
       console.error(error);
       alert('Error searching book. Try again.');
       setBooks([]);
+      setAnchorEl(null);
     }
   };
 
@@ -67,7 +76,7 @@ export const CreatePosts = () => {
     setBooks([]);
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async () => {
     const updatedBook = {
       ...bookSelected,
       type: selectedOption,
@@ -113,23 +122,33 @@ export const CreatePosts = () => {
     setSelectedCategory(1);
     setSelectedOption('reading');
     setBookSelected(initialBookState);
+    setAnchorEl(null);
   };
 
-  const ITEM_HEIGHT = 48;
-  const ITEM_PADDING_TOP = 8;
-  const MenuProps = {
-    PaperProps: {
-      style: {
-        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-        width: 250,
-      },
-    },
+  const handleAuthors = (array) => {
+    let authors = '';
+
+    if (array.length == 1) return array[0];
+
+    if (array.length > 1) {
+      for (let i = 0; i < array.length; i++) {
+        if (i == array.length - 1) {
+          authors += array[i];
+        } else {
+          authors += array[i] + ', ';
+        }
+      }
+      return authors;
+    }
   };
 
   return (
     <>
       <Button
-        sx={{ background: 'var(--chart-0)', '&:hover': { bgcolor: 'var(--chart-2)' } }}
+        sx={{
+          background: 'var(--chart-0)',
+          '&:hover': { bgcolor: 'var(--chart-2)' },
+        }}
         startIcon={<AddIcon />}
         variant="contained"
         onClick={handleOpen}
@@ -139,156 +158,181 @@ export const CreatePosts = () => {
 
       <Modal open={open} onClose={handleClose}>
         <Box
-          className="create-post"
           sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 550,
             bgcolor: 'var(--background)',
-            border: 1,
-            borderColor: 'var(--border)',
+            border: '1px solid var(--border)',
             borderRadius: '15px',
-            p: 4,
-            width: '500px',
+            boxShadow: 24,
+            p: 5,
           }}
         >
-          <FormControl sx={{ width: '400px' }}>
-            <Typography
-              sx={{ color: 'var(--text)', fontWeight: 'bold', mb: 1 }}
-            >
-              What would you like to share?{' '}
-            </Typography>
-            <Select
-              onChange={handleOnChange}
-              value={selectedOption}
-              labelId="options"
-              sx={{ width: '200px' }}
-            >
-              <MenuItem value={'reading'}>Want to Read</MenuItem>
-              <MenuItem value={'recommendation'}>Book I've Read</MenuItem>
-              <MenuItem value={'quote'}>Favorite Quote</MenuItem>
-              <MenuItem value={'review'}>Book Review</MenuItem>
-            </Select>
-            <Box
-              sx={{
-                mt: 2,
-                height: '100px',
-                alignContent: 'center',
-              }}
-            >
-              <Box sx={{ maxHeight: '60px' }}>
-                <TextField
-                  sx={{
-                    color: 'var(--text)',
-                    width: '330px',
-                    height: '55px',
-                    mr: 0.5,
-                  }}
-                  type="search"
-                  required
-                  label="Search book by title or author"
-                  value={query}
-                  onChange={(e) => setQuery(e.target.value)}
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                      e.preventDefault();
-                      handleSearch();
-                    }
-                  }}
-                />
-                <Button
-                  sx={{ height: '55px', bgcolor: 'var(--secondary)' }}
-                  onClick={handleSearch}
-                >
-                  <i className="fa-solid fa-magnifying-glass"></i>
-                </Button>
-              </Box>
-            </Box>
-            <Box>
-              {books.length > 0 && (
-                <List>
-                  {books.map((book) => (
-                    <ListItem key={book.book_id} sx={{ display: 'flex' }}>
-                      <Button
-                        onClick={() => handleBookSelected(book)}
-                        sx={{ display: 'flex', alignItems: 'center' }}
-                      >
-                        <img
-                          src={book.image}
-                          className="me-3"
-                          style={{
-                            width: '40px',
-                            height: '60px',
-                            objectFit: 'cover',
-                          }}
-                        />
-                        <Box>
-                          <Typography sx={{ mb: 1 }}>{book.title}</Typography>
-                          <Typography>{book.author}</Typography>
-                        </Box>
-                      </Button>
-                    </ListItem>
-                  ))}
-                </List>
-              )}
-            </Box>
-
-            <Box sx={{ mt: 2 }}>
-              {showContent && (
-                <>
-                  <Box sx={{ display: 'flex' }}>
-                    {selectedOption == 'quote' && categories.length > 0 && (
-                      <Box sx={{ mb: 2 }}>
-                        <Typography sx={{ fontWeight: 'bold', mb: 1 }}>
-                          Select a category:
-                        </Typography>
-                        <Select
-                          onChange={handleOnChangeCategory}
-                          value={selectedCategory}
-                          MenuProps={MenuProps}
-                        >
-                          {categories.map((category) => (
-                            <MenuItem key={category.id} value={category.id}>
-                              {category.label}
-                            </MenuItem>
-                          ))}
-                        </Select>
-                      </Box>
-                    )}
-                  </Box>
-                  <Box sx={{ mt: 2 }}>
-                    <Typography sx={{ fontWeight: 'bold', mb: 1 }}>
-                      {selectedOption == 'quote' ? 'Quote*' : 'Your Review*'}
-                    </Typography>
-                    <TextField
-                      placeholder={
-                        selectedOption == 'quote'
-                          ? 'Enter your favorite quote from this book...'
-                          : 'Share your thoughts about this book...'
-                      }
-                      value={content}
-                      onChange={(e) => setContent(e.target.value)}
-                      sx={{ width: '400px' }}
-                      multiline
-                      rows={4}
-                      required
-                    />
-                  </Box>
-                </>
-              )}
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
-              <Button
-                onClick={() => handleSubmit()}
-                variant="contained"
-                size="medium"
+          <Typography sx={{ color: 'var(--text)', fontWeight: 'bold', mb: 1 }}>
+            What would you like to share?{' '}
+          </Typography>
+          <Select
+            onChange={handleOnChange}
+            value={selectedOption}
+            labelId="options"
+            sx={{ width: 200 }}
+          >
+            <MenuItem value={'reading'}>Want to Read</MenuItem>
+            <MenuItem value={'recommendation'}>Book I've Read</MenuItem>
+            <MenuItem value={'quote'}>Favorite Quote</MenuItem>
+            <MenuItem value={'review'}>Book Review</MenuItem>
+          </Select>
+          {/* Section to search books */}
+          <Box
+            sx={{
+              mt: 2,
+              height: 100,
+              alignContent: 'center',
+            }}
+          >
+            <Box sx={{ maxHeight: 60 }}>
+              <TextField
                 sx={{
-                  bgcolor: 'var(--chart-0)',
-                  color: 'var(--background)',
-                  '&:hover': { bgcolor: 'var(--chart-2)' },
+                  color: 'var(--text)',
+                  width: 330,
+                  height: 55,
+                  mr: 0.5,
                 }}
+                type="search"
+                required
+                label="Search book by title or author"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    e.preventDefault();
+                    handleSearch(e);
+                  }
+                }}
+              />
+              <Button
+                sx={{
+                  height: 55,
+                  bgcolor: 'var(--secondary)',
+                  '&:hover': { bgcolor: 'var(--secondary-foreground)' },
+                }}
+                onClick={handleSearch}
               >
-                Submit
+                <i className="fa-solid fa-magnifying-glass"></i>
               </Button>
             </Box>
-          </FormControl>
+          </Box>
+          {/* dropdown list of books searched */}
+          {books.length > 0 && (
+            <Box
+              sx={{
+                maxHeight: 300,
+                overflowY: 'auto',
+              }}
+            >
+              <List anchorEl={anchorEl}>
+                {books.map((book) => (
+                  <ListItem key={book.book_id} sx={{ display: 'flex' }}>
+                    <ListItemButton
+                      onClick={() => handleBookSelected(book)}
+                      sx={{ display: 'flex', alignItems: 'center' }}
+                    >
+                      <img
+                        src={book.image}
+                        style={{
+                          width: 40,
+                          height: 60,
+                          objectFit: 'cover',
+                        }}
+                      />
+                      <Box sx={{ ml: 1 }}>
+                        <Typography sx={{ mb: 1 }}>{book.title}</Typography>
+                        {book.author[0] != 'N/A' && (
+                          <Typography>
+                            {`by ${handleAuthors(book.author)}`}
+                          </Typography>
+                        )}
+                      </Box>
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </List>
+            </Box>
+          )}
+          {/* section to choose Quote's category */}
+          <Box sx={{ mt: 2 }}>
+            {showContent && (
+              <>
+                <Box sx={{ display: 'flex' }}>
+                  {selectedOption == 'quote' && categories.length > 0 && (
+                    <Box sx={{ mb: 2 }}>
+                      <Typography sx={{ fontWeight: 'bold', mb: 1 }}>
+                        Select a category:
+                      </Typography>
+                      <Select
+                        onChange={handleOnChangeCategory}
+                        value={selectedCategory}
+                        MenuProps={MenuProps}
+                      >
+                        {categories.map((category) => (
+                          <MenuItem key={category.id} value={category.id}>
+                            {category.label}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </Box>
+                  )}
+                </Box>
+                {/* section to write Quote or Review */}
+                <Box sx={{ mt: 2 }}>
+                  <Typography sx={{ fontWeight: 'bold', mb: 1 }}>
+                    {selectedOption == 'quote' ? 'Quote*' : 'Your Review*'}
+                  </Typography>
+                  <TextField
+                    placeholder={
+                      selectedOption == 'quote'
+                        ? 'Enter your favorite quote from this book...'
+                        : 'Share your thoughts about this book...'
+                    }
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    sx={{ width: 400 }}
+                    multiline
+                    rows={4}
+                    required
+                  />
+                </Box>
+              </>
+            )}
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+            <Button
+              onClick={() => handleClose()}
+              variant="outlined"
+              size="medium"
+              sx={{
+                mr: 2,
+                color: 'var(--chart-1)',
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={() => handleSubmit()}
+              variant="contained"
+              size="medium"
+              sx={{
+                bgcolor: 'var(--chart-0)',
+                color: 'var(--background)',
+                '&:hover': { bgcolor: 'var(--chart-2)' },
+              }}
+            >
+              Submit
+            </Button>
+          </Box>
         </Box>
       </Modal>
     </>
